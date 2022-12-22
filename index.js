a9bef84a8562dc421c5c40ad239329626d89b592
Aakash Naik <aakashnaik150798@gmail.com>
2022-12-22 16:53:35 +0530

    added route points

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ require('dotenv').config()
 
 const app = express();
 
-app.listen(process.env.PORT, ()=>{console.log(`${process.env.env} app started at ${process.env.PORT} `)})
 
+app.get('/api/v1/bootcamps',
+(req,res)=>{console.log('get method called'); res.send('<h1>confirmed get</h1>')});
+
+app.post('/api/v1/bootcamps',
+(req,res)=>{console.log('get method called'); res.send('confirmed post')});
+
+app.delete('/api/v1/bootcamps/:id',
+(req,res)=>{console.log('get method called'); res.send(`confirmed delete id: ${req.params.id}`)});
+
+app.put('/api/v1/bootcamps/:id',
+(req,res)=>{console.log('get method called'); res.send(`confirmed put id: ${req.params.id}`)});
+
+
+app.listen(process.env.PORT, ()=>{console.log(`${process.env.env} app started at ${process.env.PORT}`)})
 
