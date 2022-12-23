cb653e280492c0f20dae9a55fb3badde4029d355
Aakash Naik <aakashnaik150798@gmail.com>
2022-12-23 13:57:13 +0530

    added controllers code COMMENT

--- controller/bootcamp.js
+++ controller/bootcamp.js
@@ -1,39 +1,39 @@
-// @desc
-// @route
-// @access
+// @desc get method for all bootcamps
+// @route GET api/v1/bootcamps
+// @access PUBLIC
 exports.getBootcamps= (req,res,next)=>{
     console.log('get method called'); 
     res.send('<h1>confirmed get</h1>');
 };
 
-// @desc
-// @route
-// @access
+// @desc create a new bootcamp
+// @route POST api/v1/bootcamps
+// @access PUBLIC
 exports.createBootcamp =(req,res,next)=>{
     console.log('post method called'); 
     res.send('confirmed post');
 };
 
-// @desc
-// @route
-// @access
+// @desc get a item in bootcamp
+// @route GET api/v1/bootcamps/:id
+// @access PUBLIC
 exports.getBootcamp= (req,res,next)=>{
     console.log('get method called'); 
     res.send(`confirmed get id: ${req.params.id}`);
 };
 
 
-// @desc
-// @route
-// @access
+// @desc update a item in bootcamp
+// @route PUT api/v1/bootcamps/:id
+// @access PUBLIC
 exports.updateBootcamp =(req,res,next)=>{
     console.log('put method called'); 
     res.send(`confirmed put id: ${req.params.id}`);
 };
 
-// @desc
-// @route
-// @access
+// @desc delete a item from bootcamps
+// @route DELETE api/v1/bootcamps/:id
+// @access PRIVATE
 exports.deleteBootcamp = (req,res,next)=>{
     console.log('delete method called'); 
     res.send(`confirmed delete id: ${req.params.id}`);
